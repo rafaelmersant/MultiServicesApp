18961cc518b446495703cc1d9d9d39cd2e8f37df
Rafael Mercedes <rafaelmersant@gmail.com>
2023-08-05 23:19:56 -0400

    Fix inventory updated after modify invoice

--- src/components/forms/invoiceForm.jsx
+++ src/components/forms/invoiceForm.jsx
@@ -798,7 +798,8 @@ class InvoiceForm extends Form {
                 detail.quantity = newQuantity;
                 await this.updateInventory(detail, true);
               } else {
-                await this.updateInventory(detail, false);
+                if (!_item)
+                  await this.updateInventory(detail, false);
               }
             }
           }
