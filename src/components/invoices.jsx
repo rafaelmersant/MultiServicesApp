d848f0ee1bfb116dd343174a41e368a5f2189afa
Rafael Mercedes <rafaelmersant@gmail.com>
2026-01-21 11:19:48 -0400

    Update to sort by invoice number instead of invoice date

--- src/components/invoices.jsx
+++ src/components/invoices.jsx
@@ -24,7 +24,7 @@ class Invoices extends Component {
     currentPage: 1,
     pageSize: 10,
     totalInvoices: 0,
-    sortColumn: { path: "creationDate", order: "desc" },
+    sortColumn: { path: "sequence", order: "desc" },
     searchParams: {
       paymentMethod: "ALL",
       customerId: 0,
